33cdcddf4f0bb7397a84c0070639227888297393
phusitsiriyakorn <phusitsiriyakorn@gmail.com>
2022-02-23 13:00:45 +0700

    HW2

---
+++ lib/main.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController username = TextEditingController();
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+  TextEditingController cnfpassword = TextEditingController();
+  TextEditingController mobile = TextEditingController();
+
+  @override
+  void initState() {
+    username.text = "";
+    email.text = "";
+    password.text = "";
+    cnfpassword.text = "";
+    mobile.text = "";
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("REGISTER"),
+          backgroundColor: Colors.blueGrey,
+        ),
+        body: Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/MyPic.jpg",
+                height: 130,
+              ),
+              SizedBox(height: 10),
+              TextField(
+                  controller: username,
+                  decoration: InputDecoration(
+                    labelText: "Username",
+                    prefixIcon: Icon(Icons.person),
+                    border: myinputborder(),
+                    enabledBorder: myinputborder(),
+                    focusedBorder: myfocusborder(),
+                  )),
+              Container(height: 10),
+              TextField(
+                  controller: email,
+                  decoration: InputDecoration(
+                    labelText: "Email Address",
+                    prefixIcon: Icon(Icons.email),
+                    border: myinputborder(),
+                    enabledBorder: myinputborder(),
+                    focusedBorder: myfocusborder(),
+                  )),
+              Container(height: 10),
+              TextField(
+                  controller: password,
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    prefixIcon: Icon(Icons.lock_open),
+                    border: myinputborder(),
+                    enabledBorder: myinputborder(),
+                    focusedBorder: myfocusborder(),
+                  )),
+              Container(height: 10),
+              TextField(
+                  controller: cnfpassword,
+                  decoration: InputDecoration(
+                    labelText: "Confirm Password",
+                    prefixIcon: Icon(Icons.lock_open),
+                    border: myinputborder(),
+                    enabledBorder: myinputborder(),
+                    focusedBorder: myfocusborder(),
+                  )),
+              Container(height: 10),
+              TextField(
+                  controller: mobile,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.phone),
+                    labelText: "Mobile",
+                    enabledBorder: myinputborder(),
+                    focusedBorder: myfocusborder(),
+                  )),
+              Container(
+                padding: const EdgeInsets.all(10.30),
+                child: TextButton(
+                  child: Text(
+                    "REGISTER",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  style: TextButton.styleFrom(
+                    primary: Colors.white,
+                    backgroundColor: Colors.blueGrey,
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  "Already have an Account ?",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  "Login",
+                  style: TextStyle(fontSize: 16, color: Colors.blueGrey),
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+
+  OutlineInputBorder myinputborder() {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        borderSide: BorderSide(
+          color: Colors.blueAccent,
+          width: 3,
+        ));
+  }
+
+  OutlineInputBorder myfocusborder() {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        borderSide: BorderSide(
+          color: Colors.redAccent,
+          width: 3,
+        ));
+  }
+}
